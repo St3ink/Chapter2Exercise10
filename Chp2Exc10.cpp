5f90683b951e59335cc2b0b6290b59a2db13cbc7
St3ink <kasonsteiner27@gmail.com>
2024-09-12 05:53:30 +0000

    Changed to allow user to input total gallons and miles driven to determine their own mpg.

--- Chp2Exc10.cpp
+++ Chp2Exc10.cpp
@@ -1,8 +1,9 @@
 /*
-Chapter 2 Exercise 10 - Distance Per Tank of Gas  
+Title: Chapter 2 Exercise 10 - Distance Per Tank of Gas  
 File Name: Chp2Exc10.cpp
-Kason Steiner
-09/2024
+Programmer: Kason Steiner
+Date: 09/2024
+
 A car holds 15 gallons of gasoline and can travel 375 miles before refueling. Write a program that calculates the number of miles per gallon the car gets. Display the result on the screen.
 Once you have coded this and placed in your main branch in GitHub, create a second branch that revises your code so that the user can enter the number of gallons and the miles driven.
 */  
@@ -12,12 +13,18 @@ using namespace std;
 
 int main() {
   
- int gallons = 15;
- int milesDriven = 375;
+  double gallons;
+  double milesDriven;
 
-int mpg = milesDriven / gallons;
+    cout << "Enter the number of gallons you vehicle holds: ";
+    cin >> gallons;
+    
+    cout << "Enter the number of miles you can drive before refueling: ";
+    cin >> milesDriven;
 
-cout << "The car gets " << mpg << " miles per gallon.\n";
+  double mpg = milesDriven / gallons;
+
+    cout << "The car gets " << mpg << " miles per gallon.\n";
   
   return 0;
 }
